2e80631129b554a9c9fa1e473c456d4201b2994f
Ayushman Paudel <mrdark2064@gmail.com>
2024-11-24 21:13:28 +0545

    final structure ready to start the project

---
+++ navigator_helper/urls.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from django.urls import path , include
+from navigator_helper import views
+urlpatterns = [
+path("map/",views.navigate_map ,name="map_navigation")
+]
--- navigator_helper/views.py
+++ navigator_helper/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def navigate_map(request):
+    return render(request,"navigator_helper/map.html")
