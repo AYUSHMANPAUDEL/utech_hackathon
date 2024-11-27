a2ebb51136ddfa4fdb1ba3bf70c79b9b42bbd704
Ayushman Paudel <mrdark2064@gmail.com>
2024-11-27 20:12:35 +0545

    few changes

--- map_roadmap/urls.py
+++ map_roadmap/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path , include
 from map_roadmap import views
 urlpatterns = [
-    path("explore", views.explore ,name="explore")
+    path("dashboard/", views.dashboard ,name="dashboard_page")
 
 
 ]
--- map_roadmap/views.py
+++ map_roadmap/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-def explore(request):
+def dashboard(request):
     return render (request, "map_roadmap/explore.html")
